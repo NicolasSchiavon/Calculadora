6286440894549b87f3aaa5543594aec252ee29cc
Nícolas <nicolasschiavon.jipa@gmail.com>
2018-10-10 10:55:21 -0400

    Nícolas; criaçao

--- Calculadora/src/br/edu/ifro/FXMLDocumentController.java
+++ Calculadora/src/br/edu/ifro/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -18,10 +19,22 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private Button btnVirgula;
+    @FXML
+    private Button btnResultado;
+    @FXML
+    private Button btnAdisao;
+    @FXML
+    private Button btnDividir;
+    @FXML
+    private Button btnSubtraçao;
+    @FXML
+    private Button btnMutiplicar;
+    @FXML
+    private Button btnApagar;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
